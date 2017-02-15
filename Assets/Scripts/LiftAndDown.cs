d3d0c28aed590f425e71383673621c97c208eb24
pengHappy <zp007@zp007deMacBook-Pro.local>
2017-02-14 20:53:34 -0800

    obstacle

---
+++ Assets/Scripts/LiftAndDown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftAndDown : MonoBehaviour {
+
+//	private bool isUp;
+	private Vector2 startPosition;
+	public float speed;
+	public float size;
+
+	// Use this for initialization
+	void Start () {
+//		isUp = true;
+		startPosition = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newPosition = Mathf.Repeat (Time.time * speed, size * 2);
+		if (newPosition >= size) {
+			transform.position = startPosition + Vector2.up * (2 * size - newPosition);
+		} 
+		else {
+			transform.position = startPosition + Vector2.up * newPosition;
+		}
+	}
+}
---
+++ Assets/Scripts/UpAndDown.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpAndDown : MonoBehaviour {
+
+	private Vector2 startPosition;
+	private Vector2 endPosition;
+	private Rigidbody2D rb2d;
+	private float deltaY;
+	public int gravityScale;
+	public float upSpeed;
+	private bool isFalling;
+
+	// Use this for initialization
+	void Start () {
+		isFalling = true;
+		deltaY = 0;
+		startPosition = this.transform.position;
+		rb2d = GetComponent<Rigidbody2D> ();
+		rb2d.gravityScale = gravityScale;
+	}
+
+	void Update() {
+		if (isFalling == false) {
+			if (transform.position.y >= startPosition.y) {
+				rb2d.isKinematic = false;
+				isFalling = true;
+				return;
+			}
+			transform.position = endPosition + deltaY * Vector2.up;
+			deltaY += Time.deltaTime * upSpeed;
+		}
+	}
+	
+	void OnTriggerEnter2D (Collider2D other) {
+//		Debug.Log ("YES");
+		rb2d.velocity = Vector2.zero;
+		rb2d.angularVelocity = 0.0f;
+		rb2d.isKinematic = true;
+		isFalling = false;
+		endPosition = transform.position;
+		deltaY = 0;
+	}
+}
