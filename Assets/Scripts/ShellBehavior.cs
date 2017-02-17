e5bbef7ef23c924408cd6a24730bf4982bd09a0d
pengHappy <zp007@zp007deMacBook-Pro.local>
2017-02-16 23:02:34 -0800

    cannon

---
+++ Assets/Scripts/ShellBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellBehavior : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	public Vector2 horizontal_velocity;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		rb2d.velocity = horizontal_velocity;
+	}
+	
+//	Update is called once per frame
+//	void Update () {
+//		
+//	}
+}
---
+++ Assets/Scripts/SpawnShell.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnShell : MonoBehaviour {
+
+	public GameObject shell;
+	public Transform shellSpawn;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (spawn ());
+	}
+	
+	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+	IEnumerator spawn() {
+		yield return new WaitForSeconds (3);
+		while (true) {
+			Instantiate (shell, shellSpawn.position, Quaternion.identity);
+			yield return new WaitForSeconds (3);
+		}
+	}
+}
