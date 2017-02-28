dbcec13c143254540795076214e362b7a6cffeb2
pengHappy <zp007@zp007deMacBook-Pro.local>
2017-02-27 19:45:46 -0800

    cannon boss

--- Assets/Scripts/ShellBehavior.cs
+++ Assets/Scripts/ShellBehavior.cs
@@ -2,19 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, yMin, yMax;
+}
+
 public class ShellBehavior : MonoBehaviour {
 
-	private Rigidbody2D rb2d;
-	public Vector2 horizontal_velocity;
+	public float dodge;
+	public float smoothing;
+	public Boundary boundary;
+	public Vector2 maneuverTime;
 
-	// Use this for initialization
-	void Start () {
+	private float currentSpeed;
+	private float targetManeuver;
+	private Rigidbody2D rb2d;
+
+	void Start() {
 		rb2d = GetComponent<Rigidbody2D> ();
-		rb2d.velocity = horizontal_velocity;
+		currentSpeed = rb2d.velocity.x;
+		StartCoroutine (Evade ());
 	}
-	
-//	Update is called once per frame
-//	void Update () {
-//		
-//	}
+
+	IEnumerator Evade() {
+		while (true) {
+			targetManeuver = Random.Range (1, dodge) * -Mathf.Sign (this.transform.position.y);
+			Debug.Log (targetManeuver);
+			yield return new WaitForSeconds (Random.Range (maneuverTime.x, maneuverTime.y));
+			targetManeuver = 0;
+//			yield return new WaitForSeconds (Random.Range (maneuverTime.x, maneuverTime.y));
+		}
+	}
+
+	void FixedUpdate() {
+		float newManeuver = Mathf.MoveTowards (rb2d.velocity.y, targetManeuver, Time.deltaTime * smoothing);
+		rb2d.velocity = new Vector2 (currentSpeed, newManeuver);
+		rb2d.position = new Vector2 (
+			Mathf.Clamp(rb2d.position.x, boundary.xMin, boundary.xMax),
+			Mathf.Clamp(rb2d.position.y, boundary.yMin, boundary.yMax)
+		);
+
+	}
+
 }
--- Assets/Scripts/SpawnShell.cs
+++ Assets/Scripts/SpawnShell.cs
@@ -6,6 +6,8 @@ public class SpawnShell : MonoBehaviour {
 
 	public GameObject shell;
 	public Transform shellSpawn;
+	public float startWait;
+	public float spawnWait;
 
 	// Use this for initialization
 	void Start () {
@@ -18,10 +20,10 @@ public class SpawnShell : MonoBehaviour {
 //	}
 
 	IEnumerator spawn() {
-		yield return new WaitForSeconds (3);
+		yield return new WaitForSeconds (startWait);
 		while (true) {
 			Instantiate (shell, shellSpawn.position, Quaternion.identity);
-			yield return new WaitForSeconds (3);
+			yield return new WaitForSeconds (spawnWait);
 		}
 	}
 }
