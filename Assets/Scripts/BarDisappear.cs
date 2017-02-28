90acf1c8e676da202f7868ce4dc117219e4a4089
pengHappy <zp007@zp007deMacBook-Pro.local>
2017-02-27 20:00:43 -0800

    bar disappear

--- Assets/Scripts/BarDisappear.cs
+++ Assets/Scripts/BarDisappear.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class BarDisappear : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("player");
+		if(other.tag == "Player") {
+			Rigidbody2D rb2d = other.GetComponent<Rigidbody2D>();
+			if (rb2d.velocity.y < 0) {
+				Debug.Log ("falling ? ");
+				Destroy (this.gameObject);
+			}
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
