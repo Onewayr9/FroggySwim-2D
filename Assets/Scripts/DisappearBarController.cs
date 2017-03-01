5a5b8f78d2ff35a492b54831c7930e44a59d4deb
Onewayr9 <Onewayr99@gmail.com>
2017-03-01 11:40:19 -0800

    Add fake ground, recreate level 1, and add mobile device support

---
+++ Assets/Scripts/DisappearBarController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearBarController : MonoBehaviour {
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			if (other.GetComponent<Rigidbody2D> ().velocity.y <= 0) {
+				gameObject.SetActive (false);
+			}
+		}
+	}
+}
