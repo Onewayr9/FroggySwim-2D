46dfb4e3cdd00da57cffcacfb90a0b8cac0122a6
Onewayr9 <Onewayr99@gmail.com>
2017-03-01 11:43:04 -0800

    Revert "bar disappear"
    This reverts commit 90acf1c8e676da202f7868ce4dc117219e4a4089.

--- Assets/Scripts/BarDisappear.cs
+++ Assets/Scripts/BarDisappear.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class BarDisappear : MonoBehaviour {
 
-	void OnTriggerEnter2D(Collider2D other) {
-		Debug.Log ("player");
-		if(other.tag == "Player") {
-			Rigidbody2D rb2d = other.GetComponent<Rigidbody2D>();
-			if (rb2d.velocity.y < 0) {
-				Debug.Log ("falling ? ");
-				Destroy (this.gameObject);
-			}
-		}
+	// Use this for initialization
+	void Start () {
+		
 	}
-
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
