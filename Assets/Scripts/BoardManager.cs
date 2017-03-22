00285c923b9970551455886a102796a451ba7f99
Onewayr9 <Onewayr99@gmail.com>
2017-03-22 11:03:20 -0700

    make background changeable as the level change

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour {
 	public GameObject[] levels;
+
+	//store backgrounds
+	//Reminder: Each level must have a background;
+	//How to build a background: 1. add a Quad in scene, reset the position. Set the scale to 17.5, 10;
+	//2. put the background image in sprites folder, set Texture type to Default, Wrap mode to repeat;
+	//3. Drag the image to Quad; 4. addBGScroller script to the quad, set the speed to 0.2
 	public GameObject[] backgrounds;
 
 	private Transform boardHolder;
