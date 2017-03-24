72a9fbacb9fd4b5c3d88652c7bb9f1c6f9d49c18
pengHappy <zp007@zp007deMacBook-Pro.local>
2017-03-24 10:42:09 -0700

    level 3

--- Assets/Scripts/obstacle/LeftAndRight.cs
+++ Assets/Scripts/obstacle/LeftAndRight.cs
@@ -10,17 +10,17 @@ public class LeftAndRight : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		startPosition = this.transform.position;
+		startPosition = this.transform.localPosition;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float newPosition = Mathf.Repeat (Time.time * speed, 2 * distance);
 		if (newPosition >= distance) {
-			transform.position = startPosition + (2 * distance - newPosition) * Vector2.right;
+			transform.localPosition = startPosition + (2 * distance - newPosition) * Vector2.right;
 		} 
 		else {
-			transform.position = startPosition + newPosition * Vector2.right;
+			transform.localPosition = startPosition + newPosition * Vector2.right;
 		}
 	}
 }
